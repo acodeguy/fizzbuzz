985a4c92cc5002b0087dc5f9d300106f9a90659d
kate102 <kate.morris.new@gmail.com>
2019-01-24 15:20:15 +0000

    Completed FizzBuzz exercise

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,12 @@
+
+def fizzbuzz(number)
+  if number % 3 == 0 &&  number % 5 == 0
+    return "fizzbuzz"
+  elsif  number % 3 == 0
+   return "fizz"
+  elsif  number % 5 == 0
+    return "buzz"
+  else
+    return number.to_s
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,22 @@
+require './lib/fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'returns "fizz" when pass 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns "buzz" when pass 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when pass 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+describe 'fizzbuzz' do
+  it 'returns itself otherwise' do
+    expect(fizzbuzz(2)).to eq '2'
   end
 end
